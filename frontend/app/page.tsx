34691093d5cbbd8a8ac79c6273ce9fe94043bd19
Jonathan Silva <jonalves.silva@jsmail.com.br>
2025-04-14 14:44:37 -0300

    feat: add section Footer

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,20 @@
+import Depoimentos from "@/sections/home/Depoimentos";
 import MultipleBanner from "@/components/MultipleBanner";
 import { bannersUrl } from "@/constants/constants";
+import MobilidadeInfo from "@/sections/home/MobilidadeInfo";
 import RentInfo from "@/sections/home/RentInfo";
+import RentInfo2 from "@/sections/home/RentInfo2";
+import Footer from "@/sections/home/Footer";
 
 export default function Home() {
   return (
     <>
-      <MultipleBanner url={bannersUrl} maxHeight={700} />
+      <MultipleBanner url={bannersUrl} maxHeight={750} />
       <RentInfo />
+      <MobilidadeInfo />
+      <RentInfo2 />
+      <Depoimentos />
+      <Footer />
     </>
   );
 }
